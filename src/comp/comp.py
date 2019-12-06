94ae7f2573f1bd9b3e6fa34ec593707a8c52b2e6
Seth Nadu <seth.nadu@gmail.com>
2019-12-06 11:16:24 -0500

    working on comp.py

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,13 +24,13 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [name.name for name in humans if name.name[0:1] is "D"]
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [name.name for name in humans if name.name[len(name.name)-1:] is "e"]
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
